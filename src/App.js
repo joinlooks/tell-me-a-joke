5f036eaf89bb50f17a39796d40865a440b1e96d8
joinlooks <joinlooks@gmail.com>
2022-11-26 18:21:06 +0530

    App works fine on localhost:3000 but doesn't work on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  const tellJoke = async () => {
+    const url = `https://joke.deno.dev/`;
+    await (await fetch(url)).json().then((result) => {
+      setData(result);
+    });
+  };
+
+  window.onload = () => {
+    tellJoke();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="joke">{data.setup}</div>
+      <div className="punchline">{data.punchline}</div>
     </div>
   );
 }
